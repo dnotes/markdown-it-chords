e1256b1cc01386de0a4a50fee41758fe20966c76
DAVID HUNT <david@Airena3.local>
2020-06-04 10:23:18 +0200

    fixed lint errors

--- readme.js
+++ readme.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
+'use strict'
 
 const fs = require('fs')
 
 const md = require('markdown-it')('commonmark')
 md.use(require('.'))
 
-let readmeText = fs.readFileSync('./README.md', {encoding: 'utf8'})
+let readmeText = fs.readFileSync('./README.md', { encoding: 'utf8' })
 let readme = md.render(readmeText)
-let styles = fs.readFileSync('./markdown-it-chords.css', {encoding: 'utf8'})
+let styles = fs.readFileSync('./markdown-it-chords.css', { encoding: 'utf8' })
 
 let output = `
 <html>
